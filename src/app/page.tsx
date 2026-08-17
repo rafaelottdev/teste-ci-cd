ff5a5e11edc4a3c4755cc01d6e580350da3a9e6e
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2026-08-17 16:36:43 -0300

    TESTE NO PUSH

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <section>TESTE CI</section>
-      <section>TESTE CI 2</section>
-      <section>TESTE CI 2.1</section>
-      <section>TESTANDO OUTRA VEZ O CI, AGORA COM UMA NOVA BRANCH</section>
+      <section>TESTE CI NO PUSH</section>
     </div>
   );
 }
