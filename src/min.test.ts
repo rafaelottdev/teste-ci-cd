e69c1ad414e62cd5f46eb99794d2b1736d3379e1
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2026-08-17 16:07:14 -0300

    fix 5

--- src/min.test.ts
+++ src/min.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from "vitest";
 import { min } from "./min";
 
 describe("min", () => {
-  it("deve somar dois números", () => {
+  it("deve diminuir dois números", () => {
     expect(min(5, 3)).toBe(1);
   });
 });
