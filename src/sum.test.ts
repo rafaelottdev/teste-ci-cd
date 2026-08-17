61c9af9e62d1c9a9f662b49e05e03b2c9bff4afe
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2026-08-17 15:30:44 -0300

    fix feat sum

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,8 +1,8 @@
-import { describe, expect, it } from "vitest"
-import { sum } from "./sum"
+import { describe, expect, it } from "vitest";
+import { sum } from "./sum";
 
 describe("sum", () => {
   it("deve somar dois números", () => {
-    expect(sum(2, 3)).toBe(6)
-  })
-})
+    expect(sum(2, 3)).toBe(6);
+  });
+});
